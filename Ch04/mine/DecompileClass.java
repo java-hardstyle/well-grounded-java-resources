3225aa46c5f7097f9d816f53aa63c8fab3db6010
Egor Kornev <boolgater@gmail.com>
2025-07-20 18:16:44 +0300

    Ch04

---
+++ Ch04/mine/DecompileClass.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DecompileClass {
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        Map<String, String> mp = new HashMap<>();
+
+        mp.put("test", "stre");
+    }
+}
